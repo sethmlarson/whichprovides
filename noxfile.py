d6ffdcb5d725a218bc665bc0b3857b8a4259fdb2
Seth Michael Larson <seth@python.org>
2025-08-20 16:27:34 -0500

    Add Python 3.14 nox session

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"])
 def tests(session):
     session.install("-e.", "pytest")
     session.run("pytest", *session.posargs)
